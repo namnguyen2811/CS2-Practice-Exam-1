8c9bc8cfeba6aea9c496922f138d7651e6ede698
killiam-cs <charlotte_killiam@milton.edu>
2024-12-13 14:06:05 -0500

    Initial commit

---
+++ src/StarshipExplorer.java
@@ -0,0 +1,5 @@
+public class StarshipExplorer {
+
+    private String alienLanguage = "Vrlnq*T xm!ow b*aF#nmpq*k^s jfy*e* *m&xlqw*e tT@br* lm*t oT%*ovnzr* oY#rwqs*y uo@L*oe^t aE!wqlm*u*r aD@rzxs* eB&vnyp Rlkzx*l W tnl@qp*e eXm*a#kyr*d aM^yplw o*e&mnbs*r eI!k*,srn* pC#txry*B cO@pb*elm Hlmpq*y P tn!*ojlwl rE#kxtm*n eF^l*cqrx tO&wtnq*e fC*!xtpm oK#xrnl nT@klpm cT@vrqp";
+
+}
